f1e3af79159da0d3783ea32357b34f900bd3cf85
kotolisic <kotolisic@gmail.com>
2020-02-17 14:11:56 +0300

    Сделал короче декодер префиксов

--- src/main.v
+++ src/main.v
@@ -12,14 +12,14 @@ initial begin $dumpfile("main.vcd"); $dumpvars(0, main); end
 // ---------------------------------------------------------------------
 // Памяти интерфейс
 // ---------------------------------------------------------------------
-reg         locked = 0;
-reg  [7:0]  ram[1048576];
+reg         ready = 0;
+reg  [31:0] ram[1048576];
 initial     $readmemh("mem.hex", ram, 20'h0000);
 
 wire [31:0] o_address;
-reg  [7:0]  i_data  = 0;
-reg  [7:0]  i_data_ = 0;
-wire [7:0]  o_data;
+reg  [31:0] i_data  = 0;
+reg  [31:0] i_data_ = 0;
+wire [31:0] o_data;
 wire        o_we;
 
 // Чтение и запись в память
@@ -27,12 +27,23 @@ always @(posedge clk) begin
 
     // Память
     i_data  <= i_data_;
-    i_data_ <= ram[ o_address ];
+    i_data_ <= ram[ o_address[31:2] ];
 
     // Запись в память
-    if (o_we) ram[ o_address ] <= o_data;
+    if (o_we) ram[ o_address[31:2] ] <= o_data;
 
 end
+
+// ---------------------------------------------------------------------
+// Тестирование
+// ---------------------------------------------------------------------
+decoder u1
+(
+    //                 F  E  D  C  B  A  9  8  7  6  5  4  3  2  1  0
+    .i_codebuf  (128'h00_00_00_00_00_00_00_00_00_00_00_00_01_66_67_26),
+    .align      (2'h1)
+);
+
 
 // ---------------------------------------------------------------------
 // Центрального процессора интерфейс
@@ -40,14 +51,14 @@ end
 
 x86cpu u0
 (
-    clock,
-    locked,
+    .clock      (clock),
+    .ready      (ready),
 
     // Память
-    o_address,
-    i_data,
-    o_data,
-    o_we
+    .o_address  (o_address),
+    .i_data     (i_data),
+    .o_data     (o_data),
+    .o_we       (o_we)
 
 );
 
